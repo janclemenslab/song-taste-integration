fdebd9aed5c4973c55eb1a7cf5c08d34cf4ed031
adpala93@gmail.com <43853502+adpala@users.noreply.github.com>
2025-07-12 17:10:14 +0200

    main commit, all current GLM inputs, code and results

---
+++ CS_mm_BWE.r
@@ -0,0 +1,37 @@
+# setwd("W:/apalaci/code/MSI_paper/r_glm_scripts")
+library(glmmTMB)
+
+# load data
+soc.data <- read.csv(file = "dat/paper_Rdf_CS_mmmf_SH.csv", header = T, stringsAsFactors = T)
+
+# select non-NA and corresponding sex
+soc.data <- subset(soc.data, (!is.na(soc.data$BWE)) & (soc.data$sex_target == "male"))
+
+# substitute zeros with small number to avoid mathematical errors later
+soc.data$BWE <- ifelse(soc.data$BWE == 0, 0.00001, soc.data$BWE)
+
+# model definitions and training
+null.soc <- glmmTMB(BWE ~ 1, data = soc.data, family = beta_family)
+red.soc <- glmmTMB(BWE ~ taste + playlist, data = soc.data, family = beta_family)
+full.soc <- glmmTMB(BWE ~ taste * playlist, data = soc.data, family = beta_family)
+# model selection
+write.csv(as.data.frame(anova(null.soc, red.soc, full.soc, test = "Chisq")), "res/CSsingle_males_BWE_chisq.csv")
+
+# coefficient tables
+null_df <- as.data.frame(summary(null.soc)$coefficients$cond)
+null_df$model <- "null_model"
+null_df$names <- row.names(null_df)
+row.names(null_df) <- NULL
+
+red_df <- as.data.frame(summary(red.soc)$coefficients$cond)
+red_df$model <- "linear"
+red_df$names <- row.names(red_df)
+row.names(red_df) <- NULL
+
+full_df <- as.data.frame(summary(full.soc)$coefficients$cond)
+full_df$model <- "nonlinear"
+full_df$names <- row.names(full_df)
+row.names(full_df) <- NULL
+
+combined_df <- rbind(null_df, red_df, full_df)
+write.csv(combined_df, "res/CSsingle_males_BWE_coeffs_table.csv")
